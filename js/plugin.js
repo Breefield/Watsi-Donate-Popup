ef7701f15ec594b740d2be845b9d4240d36c6cee
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-11 17:34:18 -0800

    Add plugin code from Codepen

---
+++ js/plugin.js
@@ -0,0 +1,23 @@
+// Extend JS arrays so we can nicely get randoms
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+}
+
+// TODO: Do not use jsonp.jit.su
+$.ajax('http://jsonp.jit.su/?url=https://watsi.org/fund-treatments.json', {
+  dataType: 'jsonp', // We have JSON, but cross domain :(
+  success: function(patients) {
+    renderProfile(patients.profiles.randomElement());
+
+    $('#watsi').addClass('loaded');
+  }
+});
+
+// TODO: No jQuery! If we can do this without it then it'll be easier
+// to install on any webpage (quicker, etc), it's a crutch for prototyping.
+function renderProfile(profile) {
+  $('#patient-title').text("Fund " + profile.name + "'s Treatment");
+  $('#patient-photo').attr('src', profile.profile_url  );
+  $('#patient-summary').text(profile. promo_description);
+  $('#more-link').attr('href', profile.url);
+}
