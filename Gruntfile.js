61ac2a0231796773f6f2d0878884c067de954cb0
Dustin Hoffman <dustin.hoffman@breefield.com>
2013-11-11 19:15:53 -0800

    Add AWS deploy for anyone :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // Used to precompile sass files
     sass: {
       dist: {
         files: [{
@@ -12,14 +13,51 @@ module.exports = function(grunt) {
         }]
       }
     },
+    
+    // Used to watch SASS files
     watch: {
       css: {
-        files: '**/*.sass',
+        files: 'css/*.sass',
         tasks: ['sass']
       }
-    }
+    },
+
+    aws: grunt.file.readJSON('./config/aws.json'),
+    's3-sync': {
+      options: {
+          key: '<%= aws.key %>'
+        , secret: '<%= aws.secret %>'
+        , bucket: '<%= aws.bucket %>'
+      },
+      s3_popup_cache: {
+          files: [
+              {
+                root: __dirname
+                , src:  'js/*.js'
+                , dest: 'v1'
+              },
+              {
+                root: __dirname
+                , src:  'css/*.css'
+                , dest: 'v1'
+              },
+              {
+                root: __dirname
+                , src:  'example.html'
+                , dest: 'v1'
+              },
+              {
+                root: __dirname
+                , src:  'img/*'
+                , dest: 'v1'
+              }
+          ]
+      },
+    },
+
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-s3-sync');
   grunt.registerTask('default', ['watch']);
 }
